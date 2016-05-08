00159c4cc9f8d4d6993c9751be071389aa9e9628
Markus <markus.kreth@web.de>
2016-05-08 09:59:10 +0200

    IndexOutOfRange Test gefunden!

--- test/de/kreth/textumbruch/business/TextUmbrecherTest.java
+++ test/de/kreth/textumbruch/business/TextUmbrecherTest.java
@@ -2,13 +2,20 @@ package de.kreth.textumbruch.business;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class TextUmbrecherTest {
 
+	private TextUmbrecher brecher;
+	
+	@Before
+	public void initTest() {
+		brecher = new TextUmbrecher();
+	}
+	
 	@Test
 	public void testEmptyString() {
-		TextUmbrecher brecher = new TextUmbrecher();
 		String result = brecher.umbruch("", 0);
 		assertNotNull(result);
 		assertTrue(result.isEmpty());
@@ -18,7 +25,6 @@ public class TextUmbrecherTest {
 	public void testLineBreak1Line(){
 		String input = "Lore ipsum lore ipsum";
 
-		TextUmbrecher brecher = new TextUmbrecher();
 		String expected = "Lore ipsum lore ipsum";
 		String result = brecher.umbruch(input, 100);
 		assertEquals(expected, result);
@@ -28,7 +34,6 @@ public class TextUmbrecherTest {
 	public void testLineBreak2Line(){
 		String input = "Lore ipsum lore ipsum";
 
-		TextUmbrecher brecher = new TextUmbrecher();
 		String expected = "Lore ipsum\nlore ipsum";
 		String result = brecher.umbruch(input, 12);
 		assertEquals(expected, result);
@@ -38,9 +43,16 @@ public class TextUmbrecherTest {
 	public void testLineBreak3Line(){
 		String input = "Lore ipsum lore ipsum lore ipsum";
 
-		TextUmbrecher brecher = new TextUmbrecher();
 		String expected = "Lore ipsum\nlore ipsum\nlore ipsum";
 		String result = brecher.umbruch(input, 12);
 		assertEquals(expected, result);
+	}
+	
+	@Test
+	public void testLineBreakWithEmptyLines() {
+		String input = "\n\t\tint lineLength = Integer.parseInt(textFieldZeichenAnzahl.getText());\n\t\ttextAreaOutput.setText(umbrecher.umbruch(textAreaInput.getText(), lineLength));";
+		String expected = "\n\t\tint lineLength = Integer.parseInt(textFieldZeichenAnzahl.getText());\n\t\ttextAreaOutput.setText(umbrecher.umbruch(textAreaInput.getText(), lineLength));";
+		String resutl = brecher.umbruch(input, 70);
+		assertEquals(expected, resutl);
 	}
 }
